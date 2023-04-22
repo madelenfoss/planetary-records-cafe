f1e6fbf3b40dc2ede1ebed370160d25b730cdf16
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-04-22 13:26:21 +0200

    Feature/to top button (#2)
    * Created empty slideshow css module, linked to main
    
    * Added 'scroll to top button' & linked to main.js
    
    * Added lorem test to check if 'to top button' works
    
    * Fixed to top button styling
    
    * Adjusted to top button width

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,3 +1,5 @@
 import menuToggle from "./modules/menuToggle.js";
+import toTopButton from "./modules/toTopButton.js";
 
 menuToggle();
+toTopButton();
---
+++ _app/js/modules/toTopButton.js
@@ -0,0 +1,19 @@
+export default function toTopButton() {
+
+	const showButtonOnPx = 100;
+	const backToTopButton = document.querySelector('.to-top__button');
+
+
+	const scrollContainer = () => {
+		return document.documentElement || document.body;
+	};
+
+	document.addEventListener('scroll', () => {
+		if (scrollContainer().scrollTop > showButtonOnPx) {
+			backToTopButton.classList.remove('hidden');
+		} else {
+			backToTopButton.classList.add('hidden');
+		}
+	})
+
+}
