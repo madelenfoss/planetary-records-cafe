f89911b496dd9ed17cea22a87e64b565ce6ca532
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-04-22 14:11:25 +0200

    Started working on Shop Items in sanity schemas

--- _studio/schemas/documents/shopItems.js
+++ _studio/schemas/documents/shopItems.js
@@ -1,3 +1,23 @@
 export default {
+	title: 'Shop item',
+	name: 'shopItem',
+	type: 'document',
+	fields: [
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string',
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			options: {
+				source: 'name'
+			}
+		},
+
+	],
+
 	
 }
--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,1 +1,5 @@
-export default []
+import shopItems from "./documents/shopItems.js";
+
+export default [
+	shopItems,
+]
