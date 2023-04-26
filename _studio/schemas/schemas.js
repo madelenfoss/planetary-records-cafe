d8f2ae6aafe836a3bdc2b65b5aa34e0d08bbbf3b
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-04-26 13:20:36 +0200

    Renamed document files and imported to schemas

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,9 +1,9 @@
-import shopItems from "./documents/shopItems.js";
+import shopItem from "./documents/shopItem.js";
 import vinylOfTheMonth from "./documents/vinylOfTheMonth.js";
 import vinylCategory from "./documents/vinylCategory.js";
 import settings from "./documents/settings.js";
 import menu from "./documents/menu.js";
-import menuItems from "./documents/menuItems.js";
+import menuItem from "./documents/menuItem.js";
 import allergen from "./documents/allergen.js";
 import staff from "./documents/staff.js";
 import album from "./documents/album.js";
@@ -12,12 +12,12 @@ import artist from "./documents/artist.js";
 
 
 export default [
-	shopItems,
+	shopItem,
 	vinylOfTheMonth,
 	vinylCategory,
 	settings,
 	menu,
-	menuItems,
+	menuItem,
 	allergen,
 	staff,
 	album,
