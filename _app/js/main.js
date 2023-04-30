7ce10169e3ed3d020d9d308b51aac1eea23ab00d
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-04-30 16:31:10 +0200

    Exported/imported shopItems.js to begin fetching

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import menuToggle from "./modules/menuToggle.js";
 import toTopButton from "./modules/toTopButton.js";
+import shopItems from "./modules/shopItems.js";
 
 menuToggle();
 toTopButton();
+shopItems();
