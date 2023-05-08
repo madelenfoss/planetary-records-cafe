19c1dea86fda62d53e9e873260d0317e0d771bf8
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-08 15:57:20 +0200

    Rendered & appended staff persons to about us page

--- _app/js/modules/staff.js
+++ _app/js/modules/staff.js
@@ -1,7 +1,7 @@
 import { sanity } from "../sanity.js";
 
 export default async function staffPersons() {
-	const query = 	`*[_type == 'staff'] | order(startDate desc) {
+	const query = 	`*[_type == 'staff'] | order(startDate asc) {
 		_id,
 		"slug": slug.current,
 		"name": name,
@@ -17,7 +17,7 @@ export default async function staffPersons() {
 	const staffContainer = document.querySelector('.about__us-staff-container');
 
 	if (staffContainer) {
-		renderHTML();
+		renderStaff();
 	}
 
 	function renderStaff() {
@@ -26,6 +26,33 @@ export default async function staffPersons() {
 			const staffImage = document.createElement('img');
 			const staffName = document.createElement('div');
 			const staffRole = document.createElement('div');
+			const photoCredits = document.createElement('div');
+			const photoCreditsStyle = document.createElement('em');
+
+			staffCard.classList.add('about__us-staff-container-person');
+			staffImage.classList.add('about__us-staff-container-person-img');
+			staffName.classList.add('about__us-staff-container-person-name');
+			staffRole.classList.add('about__us-staff-container-person-role');
+			photoCredits.classList.add('about__us-staff-container-person-credits');
+
+			staffImage.setAttribute('src', person.image);
+			staffImage.setAttribute('alt', person.altText);
+
+			staffName.innerText = `${person.name}`;
+			staffRole.innerText = `${person.role}`;
+			photoCreditsStyle.innerText = `${person.credits}`;
+
+			staffContainer.appendChild(staffCard);
+
+			photoCredits.appendChild(photoCreditsStyle);
+
+			staffCard.append(
+				staffImage,
+				staffName,
+				staffRole,
+				photoCredits
+			)
+
 		}
 	}
 
