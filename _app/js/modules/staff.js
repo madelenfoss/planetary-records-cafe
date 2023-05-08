428002325aab172e8fbb0fe65cf15df748834871
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-08 15:00:21 +0200

    Created elements for staff card, img, name & role

--- _app/js/modules/staff.js
+++ _app/js/modules/staff.js
@@ -14,6 +14,19 @@ export default async function staffPersons() {
 
 	const persons = await sanity.fetch(query);
 
-	const staffContainer = document.querySelector()
+	const staffContainer = document.querySelector('.about__us-staff-container');
+
+	if (staffContainer) {
+		renderHTML();
+	}
+
+	function renderStaff() {
+		for (const person of persons) {
+			const staffCard = document.createElement('div');
+			const staffImage = document.createElement('img');
+			const staffName = document.createElement('div');
+			const staffRole = document.createElement('div');
+		}
+	}
 
 }
