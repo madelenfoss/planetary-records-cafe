60238cab771ae1e32ec5f596c336a4158987b5a1
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-08 14:32:30 +0200

    Added query variable to staffPersons function

--- _app/js/modules/staff.js
+++ _app/js/modules/staff.js
@@ -1,6 +1,14 @@
 import { sanity } from "../sanity.js";
 
 export default async function staffPersons() {
-	const query = 
+	const query = 	`*[_type == 'staff'] | order(startDate desc) {
+		_id,
+		"slug": slug.current,
+		"name": name,
+		"started": startDate,
+		"image": image.asset->url,
+		"altText": image.alternative,
+		"role": role[]->roleName 
+	 }`
 
 }
