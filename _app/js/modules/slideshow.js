7ce5c987c8cf06f5e8eefe6340d9115fec36cfd5
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-11 21:20:28 +0200

    Started on javascript slideshow function

--- _app/js/modules/slideshow.js
+++ _app/js/modules/slideshow.js
@@ -1,3 +1,11 @@
 export default function slideshow() {
-	
+	const slideshowSlides = document.querySelectorAll('.main__slideshow-slide');
+	const buttonPrevious = document.querySelector('.main__slideshow-previous');
+	const buttonNext = document.querySelector('.main__slideshow-next');
+	const buttonDots = document.querySelectorAll('.main__slideshow-dot');
+
+	buttonPrevious.addEventListener('click', handleButtonPreviousClick);
+	buttonNext.addEventListener('click', handleButtonNextClick);
+
+	handleButtonNextClick
 }
