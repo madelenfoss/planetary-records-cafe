e9712a172177dbf5986df7297cc07fe4bba52dca
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-21 23:51:56 +0200

    Imported menuItems() to main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -5,11 +5,14 @@ import productDetails from "./modules/productDetails.js";
 import staffPersons from "./modules/staff.js";
 import slideshow from "./modules/slideshow.js";
 import vinylFilterToggle from "./modules/vinylFilterToggle.js";
+import menuItems from "./modules/menuItems.js";
 
 menuToggle();
 toTopButton();
-shopItems(); // fetchData
-productDetails(); // fetchData
+shopItems(); 
+productDetails(); 
 staffPersons();
 slideshow();
+menuItems();
 vinylFilterToggle();
+
