3a59cde086b21b4f3263090e06324dfdd6768438
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-24 16:08:05 +0200

    Started on cart function, added button click event

--- _app/js/modules/shoppingCart.js
+++ _app/js/modules/shoppingCart.js
@@ -1,9 +1,26 @@
 export default function shoppingCart() {
+	
+	// Created by following Kristiania teacher and frontend expert Alejandro Rojas' tutorial
+
 	const shoppingCart = document.querySelector('.shop__cart');
+	const addToCartButtons = document.querySelectorAll('.shop__container-item-buy');
+	const monthlyVinylAddToCartButtons = document.querySelector('.shop__container-monthly-vinyl-buy');
 
 	if (shoppingCart) {
-		
+		addToCartButtons.forEach(button => {
+			button.addEventListener('click', handleAddToCartButtonClick);
+		});
 
+		function handleAddToCartButtonClick(event) {
+			const button = event.currentTarget;
+
+			addToCart(button)
+		}
+
+		function addToCart(button) {	
+			const { id, name, price } = button.parentNode.parentNode.dataset;
+			console.log({ id, name, price });
+		}
 
 	}
 }
