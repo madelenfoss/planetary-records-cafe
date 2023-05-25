f07e48ce24e1887bf302f89f380cdfc3d5994c4d
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-25 11:24:07 +0200

    Added more to cart function

--- _app/js/modules/shoppingCart.js
+++ _app/js/modules/shoppingCart.js
@@ -1,7 +1,7 @@
 export default function shoppingCart() {
-	
-	// Created by following Kristiania teacher and frontend expert Alejandro Rojas' tutorial
 
+	// Created by following Kristiania teacher and frontend expert Alejandro Rojas' tutorial
+	const cartProducts = [];
 	const shoppingCart = document.querySelector('.shop__cart');
 	const addToCartButtons = document.querySelectorAll('.shop__container-item-buy');
 	const monthlyVinylAddToCartButtons = document.querySelector('.shop__container-monthly-vinyl-buy');
@@ -14,13 +14,39 @@ export default function shoppingCart() {
 		function handleAddToCartButtonClick(event) {
 			const button = event.currentTarget;
 
-			addToCart(button)
+			addToCart(button);
+			render();
 		}
 
 		function addToCart(button) {	
-			const { id, name, price } = button.parentNode.parentNode.dataset;
-			console.log({ id, name, price });
-		}
+			const clickedButtonDataset = button.parentNode.parentNode.dataset;
+			const product = {
+				id: clickedButtonDataset.id,
+				name: clickedButtonDataset.name,
+				price: clickedButtonDataset.price,
+				quantity: 1,
+			};
+
+			const matchInCart = cartProducts.find(product => product.id === clickedButtonDataset.id);
+			
+			if (matchInCart) {
+				matchInCart.quantity += 1;
+			} else {
+				cartProducts.push(product);
+			}
+		};
+
+		function emptyCart() {
+			cartProducts = [];
+		};
+
+		function render() {
+			if (cartProducts.length > 0) {
+
+			} else {
+				
+			}
+		};
 
 	}
 }
