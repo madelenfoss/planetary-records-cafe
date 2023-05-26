1c534cf82840beac1b64ebcceb7dcadb2eeb29ef
Madelen Foss <113188008+madelenfoss@users.noreply.github.com>
2023-05-26 11:45:12 +0200

    Created and rendered 'add to cart' functions in js

--- _app/js/modules/shoppingCart.js
+++ _app/js/modules/shoppingCart.js
@@ -1,12 +1,17 @@
 export default function shoppingCart() {
 
 	// Created by following Kristiania teacher and frontend expert Alejandro Rojas' tutorial
-	const cartProducts = [];
+	let cartProducts = [];
 	const shoppingCart = document.querySelector('.shop__cart');
+	const shopCartContentProducts = document.querySelector('.shop__cart-products');
+	const shopCartCheckoutButton = document.querySelector('.shop__cart-checkout-button');
+	const shopCartEmptyButton = document.querySelector('.shop__cart-empty-button');
+	const shoppingCartTotal = document.querySelector('.shop__cart-total');
 	const addToCartButtons = document.querySelectorAll('.shop__container-item-buy');
 	const monthlyVinylAddToCartButtons = document.querySelector('.shop__container-monthly-vinyl-buy');
 
 	if (shoppingCart) {
+
 		addToCartButtons.forEach(button => {
 			button.addEventListener('click', handleAddToCartButtonClick);
 		});
@@ -36,17 +41,60 @@ export default function shoppingCart() {
 			}
 		};
 
+		function createProductItemDOM() {
+			for (const cartProduct of cartProducts) {
+				const shopCartProduct = document.createElement('div');
+				const shopCartItemQuantity = document.createElement('div');
+				const shopCartItemTitle = document.createElement('div');
+				const shopCartItemPrice = document.createElement('div');
+	
+				shopCartProduct.classList.add('shop__cart-product');
+				shopCartItemQuantity.classList.add('shop__cart-product-quantity');
+				shopCartItemTitle.classList.add('shop__cart-product-title');
+				shopCartItemPrice.classList.add('shop__cart-product-price');
+	
+				shopCartItemQuantity.innerText = `${cartProduct.quantity}`;
+				shopCartItemTitle.innerText = `${cartProduct.name}`;
+				shopCartItemPrice.innerText = `${cartProduct.price} NOK`;
+	
+	
+				shopCartContentProducts.append(
+					shopCartProduct,
+					shopCartCheckoutButton,
+					shopCartEmptyButton 
+					)
+	
+				shopCartProduct.append(
+					shopCartItemQuantity,
+					shopCartItemTitle,
+					shopCartItemPrice
+					)
+			}
+		}
+
 		function emptyCart() {
 			cartProducts = [];
 		};
 
 		function render() {
 			if (cartProducts.length > 0) {
+				const totalItems = cartProducts.reduce((total, currentProduct) => {
+					return total + currentProduct.quantity;
+				}, 0);
+
+				shoppingCart.classList.remove('shop__cart--empty');
+				shoppingCartTotal.textContent = `${totalItems} items in cart`;
+
+				shopCartContentProducts.innerHTML = '';
+
+				for (const cartProduct of cartProducts) {
+					shopCartContentProducts += createProductItemDOM(cartProduct);
+				}
 
 			} else {
-				
+
 			}
 		};
-
 	}
+
 }
